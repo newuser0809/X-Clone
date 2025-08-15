3b56ececea31a8dc356aa25f0851b3e039f68ae4
Nguyen Duy Trong <duytrong0809@gmail.com>
2025-08-15 12:24:57 +0700

    feat: add feat arcjet for requests

---
+++ backend/src/middleware/arcjet.middleware.js
@@ -0,0 +1,42 @@
+import { aj } from "../config/arcjet.js";
+
+export const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({
+          error: "Too Many Requests",
+          message: "Rate limit exceeded. Please try again later.",
+        });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({
+          error: "Bot access denied",
+          message: "Automated requests are not allowed.",
+        });
+      } else {
+        res.status(403).json({
+          error: "Forbidden",
+          message: "Access denied by security policy.",
+        });
+      }
+    }
+    if (
+      decision.results.some(
+        (result) => result.reason.isBot() && result.reason.isSpoofed()
+      )
+    ) {
+      return res.status(403).json({
+        error: "Spoofed Bot detected",
+        message: "Malicious bot activity detected.",
+      });
+    }
+    next();
+  } catch (error) {
+    console.error("Arcject middleware error:", error);
+    next();
+  }
+};
